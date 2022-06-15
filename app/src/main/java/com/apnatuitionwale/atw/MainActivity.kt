fbeea1be82c5f0f43c88306b750b4672766bf817
krishna kumar <krishnakumarjms12@gmail.com>
2022-06-15 15:33:33 +0530

    Splash Fragment

--- app/src/main/java/com/apnatuitionwale/atw/MainActivity.kt
+++ app/src/main/java/com/apnatuitionwale/atw/MainActivity.kt
@@ -2,14 +2,10 @@ package com.apnatuitionwale.atw
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.apnatuitionwale.atw.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
+        setContentView(R.layout.activity_main)
     }
 }
