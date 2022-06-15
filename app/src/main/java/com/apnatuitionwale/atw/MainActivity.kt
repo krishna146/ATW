0ada8f5418e4aa0997e30013f66240c748329ff0
krishna kumar <krishnakumarjms12@gmail.com>
2022-06-15 13:15:04 +0530

    viewBinding added

--- app/src/main/java/com/apnatuitionwale/atw/MainActivity.kt
+++ app/src/main/java/com/apnatuitionwale/atw/MainActivity.kt
@@ -2,10 +2,14 @@ package com.apnatuitionwale.atw
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.apnatuitionwale.atw.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
     }
 }
