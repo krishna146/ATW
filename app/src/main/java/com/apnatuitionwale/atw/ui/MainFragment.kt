707833f2e6083d8e8e965343cadcad11660a7189
Prince Kumar <kumarprince99990@gmail.com>
2022-06-17 13:56:56 +0530

    navagation main fragment

--- app/src/main/java/com/apnatuitionwale/atw/ui/MainFragment.kt
+++ app/src/main/java/com/apnatuitionwale/atw/ui/MainFragment.kt
@@ -5,8 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+
 import com.apnatuitionwale.atw.R
-import com.apnatuitionwale.atw.databinding.FragmentLoginBinding
+
 import com.apnatuitionwale.atw.databinding.FragmentMainBinding
 
 
@@ -16,18 +17,38 @@ class MainFragment : Fragment() {
     private val binding: FragmentMainBinding
         get() = _binding!!
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         // Inflate the layout for this fragment
         _binding = FragmentMainBinding.inflate(inflater, container, false)
+        //return binding.root
+
+        binding.bottomNavigationView.setOnItemSelectedListener {
+            when(it.itemId){
+                R.id.course -> replaceFragment( CourseFragment())
+
+                R.id.qna -> replaceFragment(QAFragment())
+
+                R.id.profile -> replaceFragment(ProfileFragment())
+
+            }
+        true
+        }
+
         return binding.root
     }
-
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
 
+    private fun replaceFragment(fragment:Fragment) {
+        val transaction = activity?.supportFragmentManager?.beginTransaction()
+        transaction?.replace(R.id.frameLayout, fragment)
+        transaction?.disallowAddToBackStack()
+        transaction?.commit()
+    }
 }
