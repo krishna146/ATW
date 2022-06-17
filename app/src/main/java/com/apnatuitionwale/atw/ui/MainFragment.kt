74850956e027792ad0cf55eaa4b341544009086a
krishna kumar <krishnakumarjms12@gmail.com>
2022-06-17 21:28:32 +0530

    first final commit

--- app/src/main/java/com/apnatuitionwale/atw/ui/MainFragment.kt
+++ app/src/main/java/com/apnatuitionwale/atw/ui/MainFragment.kt
@@ -14,42 +14,24 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class MainFragment : Fragment() {
 
-    private var _binding : FragmentMainBinding? = null
+    private var _binding: FragmentMainBinding? = null
     private val binding: FragmentMainBinding
         get() = _binding!!
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        // Inflate the layout for this fragment
+
         _binding = FragmentMainBinding.inflate(inflater, container, false)
         //return binding.root
-
-        binding.bottomNavigationView.setOnItemSelectedListener {
-            when(it.itemId){
-                R.id.course -> replaceFragment( CourseFragment())
-
-                R.id.qna -> replaceFragment(QAFragment())
-
-                R.id.profile -> replaceFragment(ProfileFragment())
-
-            }
-        true
-        }
-
         return binding.root
     }
+
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
 
-    private fun replaceFragment(fragment:Fragment) {
-        val transaction = activity?.supportFragmentManager?.beginTransaction()
-        transaction?.replace(R.id.frameLayout, fragment)
-        transaction?.disallowAddToBackStack()
-        transaction?.commit()
-    }
 }
